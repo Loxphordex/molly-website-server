a07f7dc76681830d7ca943de7ff295959efe65e4
Silas Hallahan <silasishallahan@gmail.com>
2019-08-04 15:10:46 -0700

    patch

--- src/images/ImagesRouter.js
+++ src/images/ImagesRouter.js
@@ -9,7 +9,7 @@ ImagesRouter
     const { category } = req.query;
 
     if (!category) {
-      return res.status(400).json({ error: 'Missing category in request body' });
+      return res.status(400).json({ error: 'Missing category in request query' });
     }
 
     ImagesServices.getAllByCategory(req.app.get('db'), category)
@@ -20,6 +20,27 @@ ImagesRouter
 
         return res.status(200).json({ images });
       });
+  })
+  .patch((req, res, next) => {
+    console.log('REQ QUERY: ', req.query);
+    const { id, name } = req.query;
+
+    if (!id) {
+      return res.status(400).json({ error: 'Missing ID in request body' });
+    }
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name in request body' });
+    }
+
+    ImagesServices.renameImage(req.app.get('db'), id, name)
+      .then(image => {
+        if (!image) {
+          return res.status(400).json({ error: `Image with name ${name} and id ${id} does not exist` });
+        }
+
+        return res.status(200).json({ image });
+      })
+      .catch(next);
   });
 
 module.exports = ImagesRouter;
--- src/images/ImagesServices.js
+++ src/images/ImagesServices.js
@@ -1,9 +1,26 @@
 const ImagesServices = {
+  getById(db, id) {
+    return db('images')
+      .select('*')
+      .where('images.id', id)
+      .first();
+  },
   getAllByCategory(db, category) {
     return db('images')
       .select('*')
       .where('images.category', category);
-  }
+  },
+  renameImage(db, id, newName) {
+    return db('images')
+      .where('images.id', id)
+      .update({
+        name: newName,
+        thisKeyIsSkipped: undefined
+      })
+      .returning('*')
+      .then(([ image ]) => image)
+      .then(image => ImagesServices.getById(db, image.id));
+  },
 };
 
 module.exports = ImagesServices;
