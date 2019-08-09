b5a676c9d0fc31321f00f3fa60b07eda3f7eb50a
Silas Hallahan <silasishallahan@gmail.com>
2019-08-08 22:07:50 -0700

    e handle

--- src/app.js
+++ src/app.js
@@ -26,16 +26,16 @@ app.use('/api/images', ImagesRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  // if (NODE_ENV === 'production') {
-  //   response = { error: {message: 'server error' }};
-  // } else {
-  //   console.error(error);
-  //   response = { message: error.message, error };
-  // }
+  if (NODE_ENV === 'production') {
+    response = { error: {message: 'server error' }};
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
 
-  console.error(error);
-  response = { message: error.message, error };
-  res.status(500).json(response);
+  // console.error(error);
+  // response = { message: error.message, error };
+  // res.status(500).json(response);
 });
 
 module.exports = app;
