bd89d282cad9ef2a23d9857727cbc657202ef080
Silas Hallahan <silasishallahan@gmail.com>
2019-08-10 14:20:30 -0700

    removed registration

--- src/auth/AuthRouter.js
+++ src/auth/AuthRouter.js
@@ -12,41 +12,41 @@ AuthRouter
 
 //comment
 
-AuthRouter
-  .route('/register')
-  .post(bodyParser, (req, res, next) => {
-    const { username, password } = req.body;
+// AuthRouter
+//   .route('/register')
+//   .post(bodyParser, (req, res, next) => {
+//     const { username, password } = req.body;
 
-    if (!username) {
-      return res.status(400).json({ error: 'Missing username in request body' });
-    }
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password in request body' });
-    }
+//     if (!username) {
+//       return res.status(400).json({ error: 'Missing username in request body' });
+//     }
+//     if (!password) {
+//       return res.status(400).json({ error: 'Missing password in request body' });
+//     }
 
-    AuthServices.hasUserWithUsername(req.app.get('db'), username)
-      .then(userExists => {
-        if (userExists) {
-          return res.status(400).json({ error: 'Username already exists' });
-        }
+//     AuthServices.hasUserWithUsername(req.app.get('db'), username)
+//       .then(userExists => {
+//         if (userExists) {
+//           return res.status(400).json({ error: 'Username already exists' });
+//         }
 
-        return AuthServices.hashPassword(password)
-          .then(hashedPassword => {
-            const newUser = {
-              username,
-              password: hashedPassword,
-            };
+//         return AuthServices.hashPassword(password)
+//           .then(hashedPassword => {
+//             const newUser = {
+//               username,
+//               password: hashedPassword,
+//             };
 
-            AuthServices.insertUser(req.app.get('db'), newUser)
-              .then(user => {
-                res.status(201)
-                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                  .json({ user: AuthServices.serializeUser(user) });
-              });
-          });
-      })
-      .catch(next);
-  });
+//             AuthServices.insertUser(req.app.get('db'), newUser)
+//               .then(user => {
+//                 res.status(201)
+//                   .location(path.posix.join(req.originalUrl, `/${user.id}`))
+//                   .json({ user: AuthServices.serializeUser(user) });
+//               });
+//           });
+//       })
+//       .catch(next);
+//   });
 
 AuthRouter
   .route('/login')
